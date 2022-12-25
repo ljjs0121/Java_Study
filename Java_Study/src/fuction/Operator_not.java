76da085de2a1f2a91bb2a41f360a40ee358871fc
ljjs0121 <ljjs1215@gmail.com>
2022-12-26 04:50:15 +0900

    Switch 와 논리연산자 기초

---
+++ Java_Study/src/fuction/Operator_not.java
@@ -0,0 +1,19 @@
+package fuction;
+
+public class Operator_not {
+/*
+	 !(not) not이라고 읽는다. Boolean의 값을 역전시키는 역할을 한다. 
+	
+	true에 !를 붙이면 false가 되고 false에 !를 붙이면 true가 된다.
+ */
+	public static void main(String[] args) {
+		if (!true) {
+				System.out.println(1);
+		}
+		if (!false) {
+				System.out.println(2);
+		}
+	
+	}
+
+}
