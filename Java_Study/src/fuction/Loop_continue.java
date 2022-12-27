56494aa811bf1cb2d51a0dea9f568014f3ecb73a
ljjs0121 <ljjs1215@gmail.com>
2022-12-27 12:51:21 +0900

    반복문 공부 내용

---
+++ Java_Study/src/fuction/Loop_continue.java
@@ -0,0 +1,18 @@
+package fuction;
+
+public class Loop_continue {
+	/*
+	 *  if 조건의 참인 조건만 빼고 반복 실행
+	 */
+	public static void main(String[] args) {
+			for (int i  = 0; i < 10; i++) {
+			if (i == 5)
+					continue;            // if 조건의 참인 조건까지 반복 후 종료
+			System.out.println("Coding Everybody" +  i);
+			}
+	}
+
+}
+/* 
+ * 출력 1 2 3 4 6 7 8 9
+ */
