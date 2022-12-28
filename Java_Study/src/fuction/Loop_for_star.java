784dc90485d47f28fcd5a58d80bf7f2dfd5c8cba
ljjs0121 <ljjs1215@gmail.com>
2022-12-28 16:18:00 +0900

    for문 추가 예제

---
+++ Java_Study/src/fuction/Loop_for_star.java
@@ -0,0 +1,12 @@
+package fuction;
+
+public class Loop_for_star {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 5; i++) {
+			for (int x = 0; x <= i; x++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+	}
+}
