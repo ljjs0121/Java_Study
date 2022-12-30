eb603fad595b299469e90f8e0d531e57d8df44fb
ljjs0121 <ljjs1215@gmail.com>
2022-12-31 07:26:29 +0900

    자바의 정석 변수

---
+++ Java_jungsuk_basic/src/basic_VarEx1.java
@@ -0,0 +1,16 @@
+
+public class basic_VarEx1 {
+
+	public static void main(String[] args) {
+		int i = 30; // 정수형
+		double d = 3.14; // 실수형
+		char c = 'D'; // 문자형
+		String str = "ABCD"; // 문자열
+
+		System.out.println(i);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(str);
+	}
+
+}
---
+++ Java_jungsuk_basic/src/basic_VarEx2.java
@@ -0,0 +1,19 @@
+
+public class basic_VarEx2 {
+
+	public static void main(String[] args) {
+		final int MAX_VALUE;
+		MAX_VALUE = 100;
+//		MAX_VALUE = 200;  !! 에러
+		/*
+		 * 변수 앞에 final이 붙으면 상수가 되기 때문에 값을 변경하는 것이 허용되지 않는다. 상수의 이름은 모두 대문자로 하는것이 관례이다.
+		 * 여러 단어로 이루어져있는 경우 '_' 로 구분한다.
+		 */
+		int score = 100;
+		score = 200;
+		System.out.println("MAX_VALUE = " + MAX_VALUE);
+		System.out.println("score = " + score);
+
+	}
+
+}
