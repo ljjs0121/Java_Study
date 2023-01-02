7db4bcb00d8b6e853eb35247bedd3a51d161a66f
ljjs0121 <ljjs1215@gmail.com>
2023-01-02 11:22:35 +0900

    예제를 통한 연산자 공부

---
+++ Java_jungsuk_basic/src/OperEx7.java
@@ -0,0 +1,31 @@
+
+public class OperEx7 {
+	/*
+	 *  조건 연산자 (삼항 연산자)
+	 *  
+	 *  조건식 ? 식1 : 식2
+	 *  조건식이 참이면 식1 출력 거짓이면 식2 출력
+	 */
+	public static void main(String[] args) {
+		int x, y, z;
+		int absX, absY, absZ;
+		char signX, signY, signZ;
+		
+		x = 10;
+		y = -5;
+		z = 0;
+		
+		absX = x >= 0 ? x : -x; // x의 값이 음수이면, 양수로 만든다.
+		absY = y >= 0 ? y : -y;
+		absZ = z >= 0 ? z : -z;
+		signX = x > 0 ? '+' : (x == 0 ? ' ' : '-'); // 조건 연산자를 중첩
+		signY = y > 0 ? '+' : (y == 0 ? ' ' : '-');
+		signZ = z > 0 ? '+' : (z == 0 ? ' ' : '-');
+		
+		System.out.printf("x=%c%d%n", signX, absX);
+		System.out.printf("y=%c%d%n", signY, absY);
+		System.out.printf("z=%c%d%n", signZ, absZ);
+		// 결과값 x= +10 y= -5 z= 0
+	}
+
+}
