1357374a5797b3807bbe428fe36dad001409fb13
ljjs0121 <ljjs1215@gmail.com>
2023-01-03 16:15:14 +0900

    생활코딩 강의를 통한 java 문법 공부

---
+++ 생활코딩java/src/생활코딩/Else.java
@@ -0,0 +1,15 @@
+package 생활코딩;
+
+public class Else {
+	public static void main(String[] args) {
+		if (true) {
+				System.out.println(1);
+			} else {
+				System.out.println(2);
+			}
+
+		}
+
+// if 절 true 일때 출력 1
+//	  	false 일때 출력 2
+}
