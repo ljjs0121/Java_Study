05fa65a5b194f33d1a998823db6234c82808be2c
ljjs0121 <ljjs1215@gmail.com>
2023-01-03 15:51:13 +0900

    예제를 통한 변수 공부

---
+++ Java_jungsuk_basic/src/variable/VarEx3.java
@@ -0,0 +1,35 @@
+package variable;
+
+public class VarEx3 {
+
+	public static void main(String[] args) {
+		System.out.println("풀이하기 전의 값");
+
+		int x = 5;
+		int y = 10;
+
+		System.out.println("x =" + x);
+		System.out.println("y =" + y);
+		// x의 값을 y로 y의 값을 x로 교체할 수 있는 방법 ! 변수를 추가
+
+		System.out.println("배운 내용으로 풀이");
+
+		int tmp;
+		tmp = x; // x > tmp
+		x = y; // y > x
+		y = tmp; // tmp > y
+
+		System.out.println("x =" + x);
+		System.out.println("y =" + y);
+
+		// 간결하게 바꾸는 방법 ! 초기화
+		System.out.println("간결한 방법");
+
+		x = 10;
+		y = 5;
+
+		System.out.println("x =" + x);
+		System.out.println("y =" + y);
+	}
+
+}
---
+++ Java_jungsuk_basic/src/variable/VarEx4.java
@@ -0,0 +1,33 @@
+package variable;
+
+public class VarEx4 {
+	/*
+	 * 자바의 정석 연습문제 2-8
+	 */
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		int z = 3;
+		// x = 2 y = 3 z = 1
+		// 배운 내용으로 풀이
+		int tmp;
+		tmp = x;
+		x = y;
+		y = z;
+		z = tmp;
+
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		System.out.println("z = " + z);
+
+		// 쉬운 방법
+		x = 2;
+		y = 3;
+		z = 1;
+
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		System.out.println("z = " + z);
+	}
+
+}
