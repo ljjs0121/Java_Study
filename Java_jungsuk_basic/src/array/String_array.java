510d8d635155f7969c9b72fe5a082ecf0ea5401d
ljjs0121 <ljjs1215@gmail.com>
2023-01-05 16:42:16 +0900

    예제를 통한 String 배열 공부

--- Java_jungsuk_basic/src/array/String_array.java
+++ Java_jungsuk_basic/src/array/String_array.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class String_array {
 		/*
 		 * String 배열
-		 *  자료형의 기본값은 0 단, boolean은 true or false
+		 *  자료형의 기본값은 0 단, boolean은 false
 		 *  참조형의 기본값은 null
 		 */
 	public static void main(String[] args) {
