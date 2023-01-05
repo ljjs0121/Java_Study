adff793bab68469a5a5fa4895008e43cdf819b77
ljjs0121 <ljjs1215@gmail.com>
2023-01-05 14:39:15 +0900

    예제를 통한 String 배열 및 클래스 공부

--- Java_jungsuk_basic/src/array/String_ArrayEx1.java
+++ Java_jungsuk_basic/src/array/String_ArrayEx1.java
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 package array;
-	
+
 import java.util.Arrays;
-	
+
 public class String_ArrayEx1 {
 		/*
 		 * String 배열 
@@ -21,27 +20,4 @@ public class String_ArrayEx1 {
 	}
 	
 }
-=======
-package array;
-	
-import java.util.Arrays;
-	
-public class String_ArrayEx1 {
-		/*
-		 * String 배열 
-		 * 가위바위보 게임 예제
-		 * 
-		 */
-	public static void main(String[] args) {
-		// 인덱스 ( 0, 1, 2)
-		String[] strArr = {"가위", "바위", "보"};
-		System.out.println(Arrays.toString(strArr));
-		
-		for(int i=0; i<1; i++) {
-			int tmp = (int)(Math.random()*3); 
-			System.out.println(strArr[tmp]);
-		}
-	}
-	
-}
->>>>>>> branch 'master' of https://github.com/ljjs0121/Java_Study.git
+
