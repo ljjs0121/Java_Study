addbf1c0af7a2090e2708ae8f40a6ac8ca121232
ljjs0121 <ljjs1215@gmail.com>
2023-01-05 14:50:32 +0900

    예제를 통한 Arrays클래스 메소드 공부

--- Java_jungsuk_basic/src/array/Arrays_method.java
+++ Java_jungsuk_basic/src/array/Arrays_method.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package array;
 
 import java.util.Arrays;
@@ -30,36 +30,4 @@ public class Arrays_method {
 	}
 
 }
-=======
-package array;
 
-import java.util.Arrays;
-
-public class Arrays_method {
-	/*
-	 * Arrays 배열 다루기
-	 * equals() , toString()
-	 * 1차원일 때는 equals(), toString() 사용
-	 * 2차원일 대는 앞에 deep이 붙는다. deepequals() , deeptoString()
-	 */
-	public static void main(String[] args) {
-		int[] arr = {0,1,2,3,4};
-		int[][] arr2d = {{11,12},{21,22}};
-		
-		System.out.println(Arrays.toString(arr)); // 1차원 배열 
-		System.out.println(Arrays.deepToString(arr2d)); // 2차원 배열
-		
-		String[][] str2d1 = new String[][] {{"aaa","bbb"},{"AAA","BBB"}};
-		String[][] str2d2 = new String[][] {{"aaa","bbb"},{"AAA","BBB"}};
-		
-		System.out.println(Arrays.equals(str2d1, str2d2)); /* 일차원 배열에만 사용 가능 */
-		System.out.println(Arrays.deepEquals(str2d1, str2d2)); /* 다차원 배열에는 deepEquals 사용 */
-		
-		
-		
-	
-	
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/ljjs0121/Java_Study.git
