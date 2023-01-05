cfa7467542b8632aa7ccfc383044f155fc76dfa8
ljjs0121 <ljjs1215@gmail.com>
2023-01-05 12:28:15 +0900

    예제를 통한 2차원 배열 공부

---
+++ Java_jungsuk_basic/src/array/Two_arrayEx1.java
@@ -0,0 +1,30 @@
+package array;
+	
+public class Two_arrayEx1 {
+		/*
+		 * 2차원 배열 예제 1		  
+		 */
+	public static void main(String[] args) {
+		int[][] score = {
+				{ 100, 100, 100},
+				{ 20, 20, 20},
+				{ 30, 30, 30},
+				{ 40, 40, 40}
+		};
+		int sum = 0;
+		/*  new int[4][3] > 4행 3열
+		 	4행 > score.length
+		 	3열 > score[i].length
+		 	왠만하면 조건식에 상수를 적는 것보다 length를 적는것이 좋다.
+		*/
+		for (int i = 0; i < score.length; i++) { // score.length 의 값은 4
+			for (int j = 0; j < score[i].length; j++) { // score[i].length 의 값은 3
+				System.out.printf("score[%d][%d]=%d%n", i, j, score[i][j]);
+				
+				sum += score[i][j];		// sum = sum + score[i][j];		
+			} // 이중 배열 for문 식은 외워야 한다.
+		}
+		System.out.println("sum = "+sum);
+	}
+	
+}
