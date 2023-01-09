09e78da2a7b9728e4c0a7cd4920f82d188b7cc38
ljjs0121 <ljjs1215@gmail.com>
2023-01-09 16:09:00 +0900

    예제를 통한 참조형 반환타입 공부

---
+++ Java_jungsuk_basic/src/OOP/O_type.java
@@ -0,0 +1,23 @@
+package OOP;
+
+class Data3 { int x; }
+public class O_type {
+
+	public static void main(String[] args) {
+		Data3 d = new Data3();
+		d.x = 10;
+		
+		Data3 d2 = copy(d);
+		System.out.println("d.x ="+ d.x);
+		System.out.println("d2.x ="+ d2.x);
+	}
+	
+	static Data3 copy(Data3 d) {
+		Data3 tmp = new Data3();	//  새로운 객체 tmp 생성
+		
+		tmp.x = d.x;			// d.x의 값을 tmp.x에 복사
+		
+		return tmp;			// 복사한 객체의 주소를 반환
+	}
+
+}
